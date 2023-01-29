7ac11cc66487afd1ed6488df5d08d17c1b3477b9
TrueMolni <truemolni@gmail.com>
2023-01-29 12:04:01 +0200

    0.0.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Button from './Button';
+
+const PIXABAY_KEY = '31908525-c153f8ff1cbf36c0ec126789f';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button></Button>
+      </div>
+    );
+  }
+}
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,5 @@
+import { LoadBtn } from './Button.styled';
+
+export default function Button() {
+  return <LoadBtn>Load more</LoadBtn>;
+}
