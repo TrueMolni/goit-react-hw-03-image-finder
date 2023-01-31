bca76fd0631083983b48a19f6dae909162b2949d
TrueMolni <truemolni@gmail.com>
2023-01-31 11:41:21 +0200

    1.0.11
    закоментував файл api
    переніс все в app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,40 @@
 import { Component } from 'react';
+import axios from 'axios';
 
 import Button from './Button';
 import SearchBar from './Searchbar';
 import ImageGallery from './ImageGallery';
-import getImages from './services/Api';
+// import getImages from './services/Api';
 import Modal from './Modal';
 import ModalDetails from './ModalDetails';
 
 import { Wrapper, Message } from './App.styled';
+
+const PIXABAY_KEY = '31908525-c153f8ff1cbf36c0ec126789f';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    image_type: 'photo',
+    orientation: 'horizontal',
+    key: PIXABAY_KEY,
+    perPage: 12,
+  },
+});
+
+axios.defaults.baseURL = BASE_URL;
+
+const getImages = async (q, page) => {
+  const { data } = await instance.get(`/`, {
+    params: {
+      q,
+      page,
+    },
+  });
+  const { hits } = await data;
+  return hits;
+};
 
 export default class App extends Component {
   state = {
